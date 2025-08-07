80c3113eacac422737bf3ecce0880f053549c881
dannygoldfield <dannygoldfield@gmail.com>
2025-08-06 20:02:56 -0400

    Added mdBook to Root

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-start pt-8">
+      <h1 className="text-3xl tracking-tight">
+        Numbers
+      </h1>
+    </div>
+  );
+}
+
+export default App;
